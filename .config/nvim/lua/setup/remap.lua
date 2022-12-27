9658b1a4380dd88fef2079b976c3054480250589
sebastianselander <sebastian.selander@gmail.com>
2022-12-27 23:18:37 +0100

    shit actually work now

--- .config/nvim/lua/setup/remap.lua
+++ .config/nvim/lua/setup/remap.lua
@@ -1,8 +1,8 @@
+vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<space>", "<Nop>")
 vim.keymap.set("n", "<esc>", vim.cmd.noh)
-vim.keymap.set("n", "<leader>p", "\"_dP")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
@@ -10,12 +10,13 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "Y", "yg$")
 
-vim.keymap.set("t", "<C-c>", "<C-\\><C-n>")
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>p",  "\"_dP")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>p",  "\"_dP")
+vim.keymap.set("n", "<leader>y", [["+y]])
+vim.keymap.set("v", "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>p", [["+p]])
+
+vim.keymap.set("x", "<leader>p",  [["_dP]])
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- .config/nvim/lua/setup/set.lua
+++ .config/nvim/lua/setup/set.lua
@@ -24,7 +24,7 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
@@ -33,8 +33,6 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "auto"
 vim.opt.isfname:append"@-@"
 
-vim.opt.updatetime = 50
+vim.opt.updatetime = 100
 
 vim.opt.colorcolumn = "100"
-
-vim.g.mapleader = " "
