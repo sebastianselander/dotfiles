f3f173728c20829c02b048d8c316998bac6e046a
sebastianselander <sebastian.selander@gmail.com>
2022-12-28 15:32:57 +0100

    removed greyscale

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -230,7 +230,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
         , ((0, xF86XK_AudioRaiseVolume), spawn "pamixer -i 3")
         , ((0, xF86XK_AudioLowerVolume), spawn "pamixer -d 3")
         --lock screen
-        , ((modm .|. controlMask, xK_l), spawn "i3lock-fancy --pixelate --greyscale -f \"Iosevka-Term-Medium-Nerd-Font-Complete\"")
+        , ((modm .|. controlMask, xK_l), spawn "i3lock-fancy --pixelate -f \"Iosevka-Term-Medium-Nerd-Font-Complete\"")
         -- screenshots
         , ((0                , xK_Print), spawn "flameshot gui")
         , ((0 .|. shiftMask  , xK_Print), spawn "flameshot screen -p ~/Pictures/screenshots")
