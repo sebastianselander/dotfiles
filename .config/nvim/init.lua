7a9ef3f0a62f4114af4abafe777dfecc8ae80557
sebastianselander <sebastian.selander@gmail.com>
2023-01-14 23:44:49 +0100

    x

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,14 +1,14 @@
 require('setup')
 
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -47,6 +47,7 @@ return require('packer').startup(function(use)
         },
         -- tag = 'x.y.z' -- [^1]
     }
+
     -- Themes
     use('ayu-theme/ayu-vim')
     use('RRethy/nvim-base16')
