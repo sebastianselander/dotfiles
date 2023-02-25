84c4f9872850be6bb5fd017eff14f9454447683f
sebastianselander <sebastian.selander@gmail.com>
2023-02-25 14:36:20 +0100

    failed attempt at adding theme abandoned

--- .config/nvim/lua/themes/init.lua
+++
@@ -1,8 +0,0 @@
-local highlights = require('themes.hl')
-local function setup(opts)
-  if opts == nil then opts = { theme = "pop" } end
-  local colors = require('themes.schemes.' .. opts.theme).get_colors()
-  highlights.highlight_all(colors, opts)
-end
-
-return { setup = setup }
