994a9e65fec5e6d2595d01ae393b0edab19dc1c8
sebastian <sebastian.selander@gmail.com>
2023-03-26 00:26:01 +0100

    horizontal term

--- .config/nvim/after/plugin/toggleterm.lua
+++ .config/nvim/after/plugin/toggleterm.lua
@@ -1,11 +1,11 @@
 require("toggleterm").setup {
-    size = vim.o.columns * 0.45,
-    direction = 'float', 
+    size = 20,
+    direction = 'horizontal', 
     open_mapping = [[<C-0>]],
     hide_numbers = true, -- hide the number column in toggleterm buffers
     shade_filetypes = {},
     shading_factor = -10,
-    autochdir = false, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+    autochdir = true, -- when neovim changes it current directory the terminal will change it's own when next it's opened
     start_in_insert = true,
     insert_mappings = true,
     terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
@@ -13,6 +13,7 @@ require("toggleterm").setup {
     persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
     close_on_exit = true, -- close the terminal window when the process exits
     auto_scroll = true, -- automatically scroll to the bottom on terminal output
+    border = 'double',
     -- This field is only relevant if direction is set to 'float'
     winbar = {
         enabled = false,
