39cbbdbcc0a1a82748ada7a44b7aa3c414deb0e4
sebastian <sebastian.selander@gmail.com>
2023-05-22 23:18:55 +0200

    emaccy maybe?

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,14 +1,33 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
 ;; Don't show splash screen
 (setq inhibit-startup-message t)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+
+;; Smex or meta
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-x") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-C M-x") 'execute-extended-command)
+
+;; Disable lame shit
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+;; Ido mode
+(ido-mode 1)
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+
+;; Line numbers :)
 (global-display-line-numbers-mode 1)
 (setq display-line-numbers 'relative)
 
-(load-theme 'gruber-darker t)
+;; Initialize packages
 (package-initialize)
+
+(load-theme 'gruber-darker t)
+
+;; vimmy
 (require 'evil)
 (evil-mode 1)
